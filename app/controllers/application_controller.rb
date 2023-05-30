9623fdd3ab0abfc692174ee662c6e65dc2aa07ec
MuhamadRyans05 <ripmoxzee@gmail.com>
2023-05-30 10:51:41 +0700

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+    protected
+
+    def update_allowed_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password) }
+    end
+
+    def after_sign_in_path_for(user)
+        profile_index_path
+    end
+
+    def after_sign_out_path_for(resource)
+        new_user_session_path
+    end
+end
